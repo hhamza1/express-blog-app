3bb3ee91fb1753425fd8d19264e334f2163c9832
hhamza1 <hamzahmitou@gmail.com>
2018-08-17 22:32:14 +0300

    Data Association

--- models/post.js
+++ models/post.js
@@ -4,7 +4,14 @@ var postSchema = new mongoose.Schema({
     title: String,
     image: String,
     body: String,
-    create: {type:Date, default:Date.now}
+    create: {type:Date, default:Date.now},
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        username : String
+    }
 });
 
 module.exports = mongoose.model("Post", postSchema);
